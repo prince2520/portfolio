328569a4c9d3788be2365d0c5118757ebf257399
Prince <prince2520p@gmail.com>
2024-01-13 18:51:12 +0530

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,13 +12,18 @@ const Navbar = () => {
                 <Icon icon="ph:code-fill"/>
                 <h4>PRINCE</h4>
             </div>
-
-            <div className='flex-center navbar-routes'>
-                <h5 className='route cursor-btn'>Home</h5>
-                <h5 className='route  cursor-btn'>Blogs</h5>
+            
+            <div className='flex-center navbar-right'>
+                <div className='flex-center navbar-routes'>
+                    <h5 className='route cursor-btn'>Home</h5>
+                    <h5 className='route  cursor-btn'>Blogs</h5>
+                </div>
                 <DarkLightSwitcher/>
+                <div className='flex-center cursor-btn navbar-menu'>
+                    <Icon icon="ic:round-menu" fontSize={"2rem"}/>
+                </div>
             </div>
-
+    
         </div>
     );
 };
